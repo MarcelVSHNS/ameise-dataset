f86ddc4fe8aca0bf7ff69792f7b5cc6f681b8b3d
Marcel Vosshans <marcel.vosshans@hs-esslingen.de>
2023-10-03 16:56:11 +0200

    tests structured

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
 from PIL import ImageDraw
+import numpy as np
 from ameisedataset import core as ameise
 from ameisedataset.utils import transformation as tf
 from ameisedataset.data.names import Camera, Lidar
@@ -15,29 +13,19 @@ def plot_points_on_image(img, points, farbe="red", radius=2):
     img.show()
 
 
-infos, frames = ameise.unpack_record("samples/id00050_1696157166674880156.4mse")
+infos, frames = ameise.unpack_record("samples/frame.4mse")
 
-print(tf.transform_to_sensor(infos.lidar[Lidar.OS1_TOP].extrinsic, infos.cameras[Camera.STEREO_LEFT].extrinsic))
-x = tf.get_projection_matrix(frames[-1].lidar[ameise.Lidar.OS1_TOP], infos.lidar[ameise.Lidar.OS1_TOP], infos.cameras[Camera.STEREO_LEFT])
-
-# frames[-1].cameras[Camera.STEREO_LEFT].save("stereo_left.png")
-# frames[-1].cameras[Camera.STEREO_RIGHT].save("stereo_right.png")
+x = tf.get_projection_matrix(frames[-1].lidar[ameise.Lidar.OS1_TOP].points, infos.lidar[ameise.Lidar.OS1_TOP], infos.cameras[Camera.STEREO_LEFT])
 
 image_left = frames[-1].cameras[Camera.STEREO_LEFT]
 image_right = frames[-1].cameras[Camera.STEREO_RIGHT]
-#image_right.show()
-print(image_right.get_timestamp())
+points_top = frames[-1].lidar[Lidar.OS1_TOP]
+print(points_top.get_timestamp())
+print(np.amax(points_top.points['t']))
+
 im_rect_l = tf.rectify_image(image_left, infos.cameras[Camera.STEREO_LEFT])
 im_rect_r = tf.rectify_image(image_right, infos.cameras[Camera.STEREO_RIGHT])
 
-
-#plot_points_on_image(image_left, x)
-
-image_array = np.array(im_rect_l)
-# Konvertieren Sie RGB zu BGR
-cv2_image = image_array[:, :, ::-1]
-
-
-plot_points_on_image(im_rect_l, x)
+#plot_points_on_image(im_rect_l, x)
 
 
